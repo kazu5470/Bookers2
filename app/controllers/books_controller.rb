b532007a7f74949bb1ee7fa1ac684455bafb493b
EC2 Default User <ec2-user@ip-172-31-5-55.ap-northeast-1.compute.internal>
2022-06-21 20:09:21 +0000

    作り直し

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,46 @@
 class BooksController < ApplicationController
+  
+  def create
+    @book = current_user.book.new(params.permit(:title, :body))
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.includes(:user).page(params[:page]).per(5)
+      render :index
+    end
+  end
+  
+  
   def edit
+    @book = Book.find(params[:id])
+    redirect_to books_path if @book.user_id != current_user.id
   end
 
   def index
+    @book ||= Book.new
+    @books = Book.includes(:user).page(params[:page]).per(5)
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user    
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(params.require(:book).permit(:title, :body, :image))
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit  
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   get 'users/show'
-  get 'users/edit'
-  get 'books/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'homes/about'
-  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
